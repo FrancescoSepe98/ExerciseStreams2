c2a7e03e47500f2195774a82b13a5eee49c52e2b
FrancescoSepe98 <sepe.francesco.lavoro@gmail.com>
2022-12-01 23:57:03 +0100

    Esercizio completato

---
+++ src/WriterAndReader.java
@@ -0,0 +1,44 @@
+package src;
+
+import java.io.*;
+
+
+public class WriterAndReader {
+
+    public void write(String filePath,String s){
+        File file = new File(filePath);
+        file.canWrite();
+        if (file.exists()){
+            System.out.println("we are overwriting the file " + filePath);
+        }else{
+            System.out.println("we are creating a new file " + filePath);
+        }
+        try (
+            FileWriter writer = new FileWriter(file);
+            PrintWriter pw = new PrintWriter(writer);
+        ) {
+            pw.write(s);
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void read(String filePath){
+        File file = new File(filePath);
+        file.canExecute();
+        try (
+
+                FileReader reader = new FileReader(filePath);
+                BufferedReader br = new BufferedReader(reader);
+
+        ){ while(br.ready()){
+            String s = br.readLine();
+            System.out.println(s);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}
